c6cad00ecbe43c8b94c365cc6c453c685c971e8d
wieceslaw <vyache-nov@mail.ru>
2024-04-28 19:22:14 +0300

    added dockerfiles

---
+++ build.sh
@@ -0,0 +1,3 @@
+docker build ./washing-server -t se.ifmo.ru/washing-server:latest
+docker build ./washing-client -t se.ifmo.ru/washing-client:latest
+docker build ./washing-machine -t se.ifmo.ru/washing-machine:latest
