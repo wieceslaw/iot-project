d36ba22684405628ab9cbfc59917a47514fdabb5
wieceslaw <vyache-nov@mail.ru>
2024-04-28 16:26:43 +0300

    init

---
+++ src/test/java/ru/ifmo/se/iotproject/IotProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ifmo.se.iotproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IotProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
